836b2de5453f6460f0b041fd338b8aeb569ec1d9
xuesk <xuesk@midea.com>
2021-03-10 15:12:20 +0800

    fix 配置

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 // 实践
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['xsk', 'webpack-plugin-demo'], ' ');
+  element.classList.add('xsk-test');
+  const myIcon = new Image();
+  
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  console.log(Data);
+  console.log(Notes);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,54 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      // 数据加载 csv xml
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      // 不同格式的文件加载
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ]
+  }
 }
